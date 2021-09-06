f6c42b3297103e7e090bd3a4e4937aa33f05cdf0
Dan Thompson <daniel.24.thompson@bt.com>
2021-09-06 22:09:21 +0100

    Tidied up the admin and user home pages

--- src/main/java/com/university/skillsmatrix/web/LoginController.java
+++ src/main/java/com/university/skillsmatrix/web/LoginController.java
@@ -1,5 +1,9 @@
 package com.university.skillsmatrix.web;
 
+import com.university.skillsmatrix.service.ManagerService;
+import com.university.skillsmatrix.service.StaffService;
+import com.university.skillsmatrix.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,7 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
+    private final UserService userService;
+    private final StaffService staffService;
+    private final ManagerService managerService;
+
     @GetMapping(value = "/login")
     public String loginPage(@RequestParam(value = "error", required = false) String error,
                             Model model) {
@@ -29,12 +38,18 @@ public class LoginController {
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "/admin")
     public String adminPage(Model model){
+        String firstName = managerService.getManagerByAppUserId
+                (userService.getAppUser().getId()).getDetails().getFirstName();
+        model.addAttribute("username", String.format("Welcome %s", firstName));
         return "adminHome";
     }
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/user")
-    public String userPage(){
+    public String userPage(Model model){
+        String firstName = staffService.getStaffByAppUserId
+                (userService.getAppUser().getId()).getDetails().getFirstName();
+        model.addAttribute("username", String.format("Welcome %s", firstName));
         return "userHome";
     }
 }
